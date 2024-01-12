5b827cf8c2edb700f914dfa1c8622aecb8d568f0
Yeminson Sanchez <yeminsons@gmail.com>
2024-01-11 23:21:14 -0300

    change styles and refactor html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
 $(".card-title").click(function () {
-  $(".card-text").toggle("slow");
+    $(".card-text").toggle("slow");
 });
-$(".enviar-correo").click(function () {
-  alert("El correo fue enviado correctamente...");
+$(".send-mail").click(function () {
+    alert("El correo fue enviado correctamente...");
 });
-$(".titulo-receta").dblclick(function () {
-  $(this).css({
-    color: "red",
-  });
+$(".title-recipe").dblclick(function () {
+    $(this).css({
+        color: "red"
+    });
 });
 
-$(".titulo-receta2").dblclick(function () {
-  $(this).css({
-    color: "red",
-  });
+$(".title-recipe2").dblclick(function () {
+    $(this).css({
+        color: "red"
+    });
 });
